715d8d76d6cdb002c2aef057223f5c0ff3eea450
Evan BOISSONNOT <evan@boissonnot.fr>
2021-06-30 12:35:05 +0200

    feat(all): preparing side bar

--- starwars-game/projects/game/src/app/app.module.ts
+++ starwars-game/projects/game/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeaderComponent, SideBarComponent]
 })
 export class AppModule { }
