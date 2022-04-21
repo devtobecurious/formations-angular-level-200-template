089cd91a3db0a1e680249cc666143ec99d66eacf
Evan BOISSONNOT <evan@boissonnot.fr>
2022-04-21 14:09:25 +0200

    feat: search ok

--- starwars-game/projects/game/src/app/app.component.ts
+++ starwars-game/projects/game/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export type MonType = {
+  game: {
+    key: string,
+    config: {
+      duration: number
+    }
+  }
+}
+
 @Component({
   selector: 'game-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'game';
+
 }
--- starwars-game/projects/game/src/app/features/game/game-list/game-list.component.ts
+++ starwars-game/projects/game/src/app/features/game/game-list/game-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GameDto } from '../../../core/models/game.dto';
+import { SearchService } from '../../../shared/components/ui/search/search.service';
 import { GameService } from '../services/game.service';
 
 @Component({
@@ -9,13 +10,23 @@ import { GameService } from '../services/game.service';
   styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
+  gameState: GameDto[] = [];
   games: GameDto[] = [];
   searchItem = '';
 
-  constructor(private gameService: GameService) { }
+  constructor(private gameService: GameService,
+              private searchService: SearchService) { }
 
   ngOnInit(): void {
-    this.gameService.getAll(3).subscribe(items => this.games = items);
+    this.gameService.getAll(3).subscribe(items => {
+      this.gameState = items;
+      this.games = items;
+    });
+
+    // this.searchService.store.subscribe(item => {
+    //   this.games = this.gameState.filter(game => game.title.toLowerCase().includes(item.value.toLowerCase()));
+    // });
+
   }
 
 }
