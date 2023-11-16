bb7348dade7ee889319026625ceb7d8b35ec4335
Evan BOISSONNOT <evan@boissonnot.fr>
2023-11-16 16:32:44 +0100

    feat: concat map / merge map

--- starwars-game/projects/game/src/app/features/game/new-one/new-one.component.ts
+++ starwars-game/projects/game/src/app/features/game/new-one/new-one.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Tile } from '../../../shared/components/grid/models';
 import { TileService } from '../services/tile.service';
+import { concatMap, fromEvent, interval, map, mergeMap, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'game-new-one',
@@ -8,13 +9,41 @@ import { TileService } from '../services/tile.service';
   styleUrls: ['./new-one.component.css']
 })
 export class NewOneComponent implements OnInit {
+
   tiles: Tile[] = [];
+  @ViewChild('start', {static: true}) button !: ElementRef<HTMLButtonElement>;
 
   constructor(private tileService: TileService) { }
 
   ngOnInit(): void {
-    this.tileService.loadAll()
-    .subscribe(tiles => this.tiles = tiles);
+    let parent$ = fromEvent(this.button.nativeElement, 'click');
+    const enfant$ = interval(1000).pipe(take(100));
+
+    // parent$.subscribe(ev => {
+    //   enfant$.subscribe(item => console.info(item)); // Le nested subscribe est interdit !!!
+    // })
+    // const obs$ = parent$.pipe(
+    //   map(ev => enfant$) // converter de valeur, pas d'observable
+    // );
+
+    // obs$.subscribe({
+    //   next: obs => obs.subscribe(child => console.info(child))
+    // })
+
+    // n'attend pas le complete de l'enfant
+    const obs$ = parent$.pipe(
+      mergeMap(ev => enfant$) // subscribe sur l'enfant et next => même comportement de subscribe de subscribe
+    );
+
+    // const obs$ = parent$.pipe(
+    //   concatMap(ev => enfant$) // subscribe sur l'enfant et next => même comportement de subscribe de subscribe
+    // );
+
+    obs$.subscribe(tick => console.info(tick));
+
+
+    // this.tileService.loadAll()
+    // .subscribe(tiles => this.tiles = tiles);
   }
 
 }
