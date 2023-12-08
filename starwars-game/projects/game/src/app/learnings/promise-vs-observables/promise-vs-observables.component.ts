270065914ecf862ebc7ac685be1b0759b328a320
Evan BOISSONNOT <evan@boissonnot.fr>
2023-12-08 11:30:18 +0100

    feat: end obs

--- starwars-game/projects/game/src/app/learnings/promise-vs-observables/promise-vs-observables.component.ts
+++ starwars-game/projects/game/src/app/learnings/promise-vs-observables/promise-vs-observables.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, shareReplay } from 'rxjs';
+import { Observable, finalize, shareReplay, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'game-promise-vs-observables',
@@ -10,7 +11,18 @@ import { Observable, shareReplay } from 'rxjs';
   styleUrls: ['./promise-vs-observables.component.css']
 })
 export class PromiseVsObservablesComponent {
+  // private http = inject(HttpClient);
+
   ngOnInit(): void {
+    // const livres$ = this.http.get('sdsdsds');
+
+    // livres$.subscribe({
+    //   next: items => {
+
+    //   }
+    // })
+
+
     console.info('---------');
 
     const callBackAExecuter = () => console.info('Ah que coucou');
@@ -28,19 +40,35 @@ export class PromiseVsObservablesComponent {
 
     const obs$ = new Observable<string>(observer => { // LAZY
       console.info('00 - Observable'); // SYNC
-      observer.next('O - coucou');
-      observer.next('1 - coucou');
-      observer.next('2 - coucou'); // SYNC
-    });
-    const obs2$ = obs$.pipe(shareReplay(2));
 
-    //obs$.subscribe(callBackPourPromsseEtObservable);
-    obs2$.subscribe({
-      next: callBackPourPromsseEtObservable
+      observer.next('O - coucou');
+      setTimeout(() => {
+        observer.next('1 - coucou');
+        observer.complete();
+      }, 0);
+      observer.next('2 - coucou'); // SYNC
+
     });
-    obs2$.subscribe({
+
+    obs$.pipe(
+      tap(item => console.info('tap', item)),
+      tap(item => console.info('tap2', item)),
+      finalize(() => console.info('la fin'))
+    ).subscribe({
       next: callBackPourPromsseEtObservable
-    });
+    })
+
+    // const obs2$ = obs$.pipe(shareReplay(2));
+
+    // //obs$.subscribe(callBackPourPromsseEtObservable);
+    // obs2$.subscribe({
+    //   next: callBackPourPromsseEtObservable,
+    //   complete: () => console.info('Hey cest fini')
+    // });
+    // obs2$.subscribe({
+    //   next: callBackPourPromsseEtObservable,
+    //   complete: () => console.info('Hey cest fini 2')
+    // });
 
     console.info('***********');
   }
