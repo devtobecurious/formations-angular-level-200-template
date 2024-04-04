06e22de0ca4ac816bf5c1b2fda2d4aff5b73969d
Evan BOISSONNOT <evan@boissonnot.fr>
2024-04-04 17:23:30 +0200

    feat: fin 2nde jrnée

--- starwars-game/projects/game/src/app/features/statistics/components/list-statistics/list-statistics.component.spec.ts
+++ starwars-game/projects/game/src/app/features/statistics/components/list-statistics/list-statistics.component.spec.ts
@@ -2,22 +2,37 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListStatisticsComponent } from './list-statistics.component';
 
-describe('ListStatisticsComponent', () => {
+fdescribe('ListStatisticsComponent', () => {
   let component: ListStatisticsComponent;
   let fixture: ComponentFixture<ListStatisticsComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async () => { // prépare un module de testing, comme si c'était un module angular
     await TestBed.configureTestingModule({
       imports: [ListStatisticsComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ListStatisticsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance;// permet de créer une instance de notre composant
+    // fixture.detectChanges();
+    fixture.autoDetectChanges(true);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // vérifie si instance pas null
+  });
+
+  it('should display a table with 3 rows', () => {
+    // Arrange
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    const domGenereDeMonComposant = fixture.nativeElement;
+    const table = domGenereDeMonComposant.querySelector('table');
+
+    expect(table).toBeTruthy();
+    expect(table.rows.length).toBe(3);
   });
 });
--- starwars-game/projects/game/src/app/features/statistics/components/list-statistics/list-statistics.component.ts
+++ starwars-game/projects/game/src/app/features/statistics/components/list-statistics/list-statistics.component.ts
@@ -1,12 +1,13 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'game-list-statistics',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './list-statistics.component.html',
   styleUrl: './list-statistics.component.css'
 })
 export class ListStatisticsComponent {
-
+  stats = [{ annee: 2024 }, {annee: 2023}, {annee: 2020}]
 }
