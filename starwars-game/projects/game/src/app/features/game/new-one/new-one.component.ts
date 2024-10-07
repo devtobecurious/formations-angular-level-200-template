be0461157b5534fba20774dc87ccfcd5f0790a75
Evan BOISSONNOT <evan@boissonnot.fr>
2024-10-07 16:13:59 +0200

    feat: let et async

--- starwars-game/projects/game/src/app/features/game/new-one/new-one.component.ts
+++ starwars-game/projects/game/src/app/features/game/new-one/new-one.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
 import { Tile } from '../../../shared/components/grid/models';
 import { TileService } from '../services/tile.service';
 import { GridComponent } from '../../../shared/components/grid/grid.component';
+import { concatMap, fromEvent, interval, mergeMap, Observable, switchMap, take, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'game-new-one',
     templateUrl: './new-one.component.html',
     styleUrls: ['./new-one.component.css'],
     standalone: true,
-    imports: [GridComponent]
+    imports: [GridComponent, AsyncPipe]
 })
 export class NewOneComponent implements OnInit {
+  btnStart = viewChild<ElementRef<HTMLButtonElement>>('btnStart')
+  timer$: Observable<number> | undefined
+  private readonly child$ = interval(1000).pipe(take(10))
   tiles: Tile[] = [];
 
   constructor(private tileService: TileService) { }
 
   ngOnInit(): void {
+    this.timer$ = fromEvent(this.btnStart()!.nativeElement, 'click').pipe(
+      tap(evt => console.info(evt)),
+      // mergeMap(() => this.child$) // quel est le comportement d'un merge map
+      // concatMap(() => this.child$)
+      switchMap(() => this.child$)
+    )
+
     this.tileService.loadAll()
     .subscribe(tiles => this.tiles = tiles);
   }
