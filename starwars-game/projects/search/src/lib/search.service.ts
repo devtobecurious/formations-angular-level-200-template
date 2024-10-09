75e0aa7563a511c41beb3a7b749647824e3c823d
Evan BOISSONNOT <evan@boissonnot.fr>
2024-10-09 17:16:25 +0200

    feat: fin formation

--- starwars-game/projects/search/src/lib/search.service.ts
+++ starwars-game/projects/search/src/lib/search.service.ts
@@ -1,19 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { initialValue, SearchState } from './store';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root' // TreeShaking
 })
 export class SearchService {
   // Et si on passait aux signals ?
-  private readonly store = new BehaviorSubject<SearchState>(initialValue)
+  // private readonly store = new BehaviorSubject<SearchState>(initialValue)
+  private readonly store = signal<SearchState>(initialValue)
 
   dispatch(item: SearchState): void {
-    this.store.next({ ...item }) // on controle la valeur de l'état
+    this.store.set({ ...item }) // on controle la valeur de l'état
   }
 
   get asObservable(): Observable<SearchState> {
-    return this.store.asObservable() // on ne peut que s'abonner
+    return toObservable(this.store) // on ne peut que s'abonner
   }
 }
--- workspace/projects/test-app/src/app/app.component.ts
+++ workspace/projects/test-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal, untracked } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,38 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test-app';
+  title = 'test-app'
+
+  counter$$ = signal<number>(0)
+
+  count(): void {
+    // this.counter.update(() => this.counter() + 1)
+  }
+
+  title$$ = signal<string>('test-app')
+  titleMaj$$ = computed(() => this.title$$().toUpperCase())
+
+  counterEffect = effect(() => {
+    const titleResult = this.title$$()
+    const counterResult = untracked(this.counter$$)
+    console.info('effect', titleResult, counterResult)
+  })
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.title = '1. Star wars - BO'
+    }, 1500)
+
+    setTimeout(() => {
+      this.title$$.set('2. Star wars - BO')
+    }, 2500);
+
+    setTimeout(() => {
+      this.counter$$.set(100)
+    }, 3500);
+  }
+
+  click(): void {
+    console.info('ça refresh meme si on n\'est plus dans la zone :\'(')
+  }
 }
