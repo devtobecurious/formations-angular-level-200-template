a220bf7a066dbef6e69de5c0164be728b3f2e91d
Evan BOISSONNOT <evan@boissonnot.fr>
2024-11-21 15:18:20 +0100

    feat: share replay

--- starwars-game/projects/game/src/app/features/game/services/game.service.ts
+++ starwars-game/projects/game/src/app/features/game/services/game.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { api } from 'projects/game/src/environments/api';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay } from 'rxjs';
 import { GameDto } from '../../../core/models/game.dto';
 
 @Injectable()
-export class GameService {
+export class BaseService<T extends { id: number }> {
+  private list$ = this.httpClient.get<T[]>(api.games.url + '?max=' + 10).pipe(shareReplay(1)); // LAZY
 
   constructor(private readonly httpClient: HttpClient) { }
 
-  getAll(nbItems: number = 100): Observable<GameDto[]> {
-    return this.httpClient.get<GameDto[]>(api.games.url + '?max=' + nbItems);
+  getAll(nbItems: number = 100): Observable<T[]> {
+    return this.list$
   }
+}
+
+@Injectable()
+export class GameService extends BaseService<GameDto> {
 }
