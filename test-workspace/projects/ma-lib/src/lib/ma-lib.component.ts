b01c7251e106a698d4ebc8ad62e8d91402b761af
Evan BOISSONNOT <evan@boissonnot.fr>
2024-11-25 14:42:38 +0100

    feat: notion de lib feat: rappel ts

---
+++ test-workspace/projects/ma-lib/src/lib/ma-lib.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ma-lib',
+  imports: [],
+  template: `
+    <p>
+      ma-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class MaLibComponent {
+
+}
---
+++ test-workspace/projects/ma-lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ma-lib
+ */
+
+export * from './lib/ma-lib.service';
+export * from './lib/ma-lib.component';
