c230095d40e7213eefda4b4b59c434e084a6272c
evan boissonnot <evan@boissonnot.fr>
2025-07-02 16:29:58 +0200

    feat: kill angular :D

--- starwars-game/projects/game/src/app/app.component.ts
+++ starwars-game/projects/game/src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'game';
+
+  killAngular() {
+    setTimeout(() => {
+      console.info('coucou ??')
+    }, 0);
+  }
 }
