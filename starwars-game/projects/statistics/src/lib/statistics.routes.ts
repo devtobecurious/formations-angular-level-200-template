1cf921b583a5db18dbd69adfef15246730c33c22
evan boissonnot <evan@boissonnot.fr>
2025-07-02 17:20:42 +0200

    feat: lazy loading

---
+++ starwars-game/projects/statistics/src/lib/statistics.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { StatisticsComponent } from "./statistics.component";
+
+export const statsRoutes: Routes = [
+  {
+    path: '',
+    component: StatisticsComponent
+  }
+]
--- starwars-game/projects/statistics/src/public-api.ts
+++ starwars-game/projects/statistics/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of statistics
  */
 
+export * from './lib/statistics.routes';
 export * from './lib/store/statistics.store';
 export * from './lib/statistics.component';
